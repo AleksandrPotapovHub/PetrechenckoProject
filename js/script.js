cef56062e4008d75d2961f4da0e1dd2fdcd58997
AleksandrPotapovHub <129421135+AleksandrPotapovHub@users.noreply.github.com>
2023-04-02 11:31:31 +0300

    Решение практики 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '10');
+
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+console.log(personalMovieDB);
+
+const a = prompt('Назовите один из последних просмотренных фильмов', 'Телохранитель жены киллера'),
+			b = +prompt('Насколько вы его оцените', '8.1'),
+			c = prompt('Назовите один из последних просмотренных фильмов', 'Телохранитель жены киллера 2'),
+			d = +prompt('Насколько вы его оцените', '8.3');
+
+console.log(personalMovieDB.movies);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
